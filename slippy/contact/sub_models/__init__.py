b6e96406e33e3dbfad7a1d910741f89ff4429ba2
mikeWshef <mike.watson@sheffield.ac.uk>
2022-02-24 14:53:45 +0000

    Added sub mode to shift rolling surface

--- slippy/contact/sub_models/__init__.py
+++ slippy/contact/sub_models/__init__.py
@@ -28,7 +28,9 @@ from .rigid_body_displacement import RollingSliding1D, RigidBodyDisplacementSlid
 from .dummy_value import DummyValue
 from .fill_displacements import FillDisplacements
 from ._TransientSubModelABC import _TransientSubModelABC
+from .shift_surface import UpdateShiftRollingSurface
 
 __all__ = ['ResultContactStiffness', 'WearElasticPerfectlyPlastic', 'FrictionCoulombSimple', 'TangentialPureSliding',
            'TangentialPartialSlip', 'SubsurfaceStress', 'RigidBodyDisplacementSliding',
-           'RollingSliding1D', 'DummyValue', '_TransientSubModelABC', 'FillDisplacements']
+           'RollingSliding1D', 'DummyValue', '_TransientSubModelABC', 'FillDisplacements',
+           'UpdateShiftRollingSurface']
